008d1ac2d6ee8ad139f0ddcd2b86d996b21899d1
amamemie <luisrojas10927406@gmail.com>
2026-06-23 10:28:21 -0400

    Update

--- Geometry/Point.cpp
+++ Geometry/Point.cpp
@@ -12,8 +12,8 @@ struct point
   	point operator *(int a) {return point(x * a, y * a);}
 	bool operator <(const point& p) const {return x == p.x ? y < p.y : x < p.x;}
 	int left(point a, point b) {
-        return ((b - a) ^ (*this - a)); // 0, -, +
-    }
+          return ((b - a) ^ (*this - a));
+        }
 };
 ostream& operator<<(ostream& os, const point& p) {
     return os << "(" << p.x << "," << p.y << ")\n";
